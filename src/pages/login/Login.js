c729229c918c8b17a64c05659f6319036c058cc9
Trinh Tan Tai <fxqtai@gmail.com>
2020-10-31 15:58:03 +0700

    Login register using hook , axios receive response

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import Axios from 'axios';
+import React, { useState } from 'react';
 import loginImg from "../../login.svg";
+import { message } from 'antd';
+import { useHistory } from "react-router";
 
 function Login(props) {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [statusLogin, setStatusLogin] = useState('');
+    const history = useHistory();
+
+    const login = (e) => {
+      // e.preventDefault();
+      // fetch('http://localhost:5000/users/login', {
+      //  method: 'post',
+      //  headers: {'Content-Type':'application/json'},
+      //  body: JSON.stringify({
+      //   "username": username,
+      //   "password": password
+      //  })
+      // }).then((response) => {
+      //  let data =  response.json();
+      //  console.log(data);
+      // }).then((data) =>{
+      //   console.log('PING');
+      //   console.log(data);
+      // }).catch(err => {
+      //   console.log("ERROR: " + err);
+      // });
+      Axios.post('http://localhost:5000/users/login',{
+        "username": username,
+         "password": password
+      }).then((response) => {
+         if(response.data == false){
+          message.error('Login fail! Wrong username / password!!');
+         }
+         else{
+          message.success('Login successfully');
+          history.push({
+            pathname:  "/myboards",
+            state: {
+              response: "myboards page" 
+            } 
+         });
+         }
+         console.log(response);
+        });
+    }
+
+    
+
     return (
         <div className="base-container" ref={props.refContainer} >
         <div className="header">Login</div>
@@ -12,16 +61,20 @@ function Login(props) {
           <div className="form">
             <div className="form-group">
               <label htmlFor="username">Username</label>
-              <input type="text" name="username" placeholder="username" />
+              <input type="text" name="username" placeholder="username" onChange={(e) => {
+                  setUsername(e.target.value);
+              }}/>
             </div>
             <div className="form-group">
               <label htmlFor="password">Password</label>
-              <input type="password" name="password" placeholder="password" />
+              <input type="password" name="password" placeholder="password"onChange={(e) => {
+                  setPassword(e.target.value);
+              }} />
             </div>
           </div>
         </div>
         <div className="footer">
-          <button type="button" className="btn">
+          <button type="button" className="btn" onClick={login}>
             Login
           </button>
         </div>
--- src/pages/login/Register.js
+++ src/pages/login/Register.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import loginImg from "../../login.svg";
 
 function Register(props) {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const register = () => {
+    
+    fetch('http://localhost:5000/users/register', {
+     method: 'post',
+     headers: {'Content-Type':'application/json',
+              'Accept': 'application/json'  
+      },
+     body: JSON.stringify({
+      "username": username,
+      "password": password
+     })
+    }).then((response) => {
+      console.log(response);
+    });
+  }
+
     return (
         <div className="base-container" ref={props.refContainer}>
         <div className="header">Register</div>
@@ -12,7 +32,9 @@ function Register(props) {
           <div className="form">
             <div className="form-group">
               <label htmlFor="username">Username</label>
-              <input type="text" name="username" placeholder="username" />
+              <input type="text" name="username" placeholder="username" onChange={(e) => {
+                  setUsername(e.target.value);
+              }} />
             </div>
             <div className="form-group">
               <label htmlFor="email">Email</label>
@@ -20,12 +42,14 @@ function Register(props) {
             </div>
             <div className="form-group">
               <label htmlFor="password">Password</label>
-              <input type="text" name="password" placeholder="password" />
+              <input type="text" name="password" placeholder="password" onChange={(e) => {
+                  setPassword(e.target.value);
+              }}/>
             </div>
           </div>
         </div>
         <div className="footer">
-          <button type="button" className="btn">
+          <button type="button" className="btn" onClick ={register}>
             Register
           </button>
         </div>
